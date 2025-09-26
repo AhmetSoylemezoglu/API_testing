2d74a617885c458296c8b7ec0f1294753f0e5ed9
Ahmet Yusuf Söylemezoğlu <ahmetys2005@gmail.com>
2025-09-26 17:30:28 +0300

    Create deneme.py

---
+++ deneme.py
@@ -0,0 +1,25 @@
+#nicegui modülünü deneyecem
+from nicegui import ui
+
+def chng():
+    a.text+="yazı değişti"
+
+
+
+ui.page_title("benim sitem")
+a = ui.label("merhaba dünya 2")
+ui.label("2. yazı bu da")
+
+
+
+ui.button("bana bas", on_click=chng)
+
+
+
+
+
+
+
+
+
+ui.run()
